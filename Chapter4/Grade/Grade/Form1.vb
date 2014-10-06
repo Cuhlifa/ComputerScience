ce69412653270ec011a8503305ee5ba01f174cc4
Cuhlifa <bosscrypt@hotmail.com>
2014-10-05 21:11:59 -0400

    fddfd

---
+++ Chapter4/Grade/Grade/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+
+    Dim fail As Integer
+    Dim pass As Integer
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.Text.ToLower.Equals("a") Or TextBox1.Text.ToLower.Equals("b") Or TextBox1.Text.ToLower.Equals("c") Or TextBox1.Text.ToLower.Equals("d") Then
+            pass += 1
+        Else
+            fail += 1
+        End If
+        Label2.Text = String.Concat(pass)
+        Label3.Text = String.Concat(fail)
+    End Sub
+End Class
---
+++ Chapter4/PhoneBill/PhoneBill/Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim price As Integer = 25
+
+        If CheckBox1.Checked = True Then
+            price += 3.5
+        End If
+        If CheckBox2.Checked = True Then
+            price += 3.5
+        End If
+        If CheckBox3.Checked = True Then
+            price += 3.5
+        End If
+
+        Label1.Text = String.Concat(price)
+
+    End Sub
+End Class
