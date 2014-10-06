7dad4cca5d8d81d415d70a89b1c0a512cc1e9874
Cuhlifa <bosscrypt@hotmail.com>
2014-10-05 20:37:04 -0400

    trtr

---
+++ Chapter4/PackageCheck/PackageCheck/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim heavy As Boolean = False
+        Dim big As Boolean = False
+
+        If Integer.Parse(TextBox1.Text) > 27 Then
+            heavy = True
+        ElseIf Integer.Parse(TextBox2.Text) * Integer.Parse(TextBox3.Text) * Integer.Parse(TextBox4.Text) > 100000 Then
+            big = True
+        End If
+
+        If heavy And big Then
+            Label5.Text = "Status: " + "Both"
+        ElseIf heavy Then
+            Label5.Text = "Status: " + "heavy"
+        ElseIf big Then
+            Label5.Text = "Status: " + "big"
+        Else
+            Label5.Text = "Status: " + "Good"
+        End If
+
+    End Sub
+End Class
---
+++ Chapter4/TroubleShoot/TroubleShoot/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.Text.ToLower.Equals("n") And TextBox1.Text.ToLower.Equals("n") Then
+
+        End If
+    End Sub
+End Class
